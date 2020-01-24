c3bcf4d00f172f5fa900bf08666e50919d8d9afb
Liza Star <lizadstar18@gmail.com>
2020-01-23 18:52:05 -0600

    added array and objects and started dom

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('Pet-adoption');
+const pet [
+{image:
+name:'Chai',
+color:'White and Tan',
+specialSkill:'Running fast in hallways',
+typeOfPet:'Dog'},
+
+{image:
+name:"Kiki",
+color:'Orange',
+specialSkill:'Scratches furniture',
+typeOfPet:'Cat'},
+
+{image:
+name:'Genny'
+color:'black'
+specialSkill:'rolling in a ball'
+typeOfPet:'hamster'},
+
+{image:
+name:'Barney'
+color:'purple'
+specialSkill:'master of imagination'
+typeOfPet:'Dinosaur'},
+
+{image:
+name:'Kermit'
+color:'lime green'
+specialSkill:'leaping through the water'
+typeOfPet:'Frog'}
+
+{image:
+name:'Bunny'
+color:'grey'
+specialSkill:'carrot connoisseur'
+typeOfPet:'rabbit'}
+]
