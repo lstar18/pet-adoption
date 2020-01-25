a9829dc7e33948caa92cbd589e7f356859d23b53
Liza Star <lizadstar18@gmail.com>
2020-01-25 10:26:17 -0600

    Updated sizing within cards, made buttons and colored them correctly, styilng updates

--- main.js
+++ main.js
@@ -11,27 +11,27 @@ color:'Orange',
 specialSkill:'Scratches furniture',
 typeOfPet:'Cat'},
 
-{image:"https://hamstersportal.com/wp-content/uploads/2019/12/Black-Bear-Hamster.jpg",
+{image:"https://res.cloudinary.com/twenty20/private_images/t_watermark-criss-cross-10/v1558372159000/photosp/1ae76d0e-4f4d-496f-9262-2c898ebff2a5/stock-photo-dog-sleeping-labrador-puppy-cute-pet-puppies-dogs-sleep-1ae76d0e-4f4d-496f-9262-2c898ebff2a5.jpg",
 name:'Genny',
-color:'black',
-specialSkill:'Transforming into hamsters',
+color:'Black',
+specialSkill:'Sleeping like a baby',
 typeOfPet:'Dog'},
 
 {image:"https://i.pinimg.com/originals/67/ef/9e/67ef9ee912cca4053c1dad6351dbf994.jpg",
 name:'Barney',
-color:'purple',
-specialSkill:'master of imagination',
+color:'Purple',
+specialSkill:'Master of imagination',
 typeOfPet:'Dinosaur'},
 
-{image:"https://le-www-live-s.legocdn.com/sc/media/lessons/wedo-2/wedo-projects/images/frogs-metamorphosis-project-image-feb9db40c70bcda57e12f5671d4bc278.jpg?fit=inside|1400:1013",
+{image:"https://icatcare.org/app/uploads/2019/09/The-Kitten-Checklist-1.png",
 name:'Kimmy',
-color:'lime green',
-specialSkill:'Transforming into Frogs',
+color:'Tan, Black, and White',
+specialSkill:'Stealing hearts',
 typeOfPet:'Cat'},
 
 {image:"https://en.es-static.us/upl/2018/06/Screen-Shot-2018-06-27-at-2.30.15-PM.png",
 name:'Timmy',
-color:'Dinosaur colors',
+color:'Green and Tan',
 specialSkill:'Knitting blankets',
 typeOfPet:'Dinosaur'}
 ];
@@ -45,12 +45,23 @@ const buildPetCards = () => {
     let domString = '';
     for(let i= 0; i <pets.length; i++) {
         domString += `<div class="cards">`;
-        domString += `<h2 class="name">${pets[i].name}</h2>`;
+        domString += `<h2 class="name"> ${pets[i].name}</h2>`;
         domString += `<img src="${pets[i].image}" alt="A picture of a ${pets[i].typeOfPet}" class="center">`;
-        domString += `<p class="color">${pets[i].color}</p>`;
-        domString += `<p class="specialskills">${pets[i].specialSkill}</p>`;
-        domString += `<footer class"typeofpet">${pets[i].typeOfPet}</footer>`;
-        domString += `</div>`;
+        domString += `<p class="color"> <strong>Color:</strong> ${pets[i].color}</p>`;
+        domString += `<p class="specialskills"> <strong> Skills:</strong> ${pets[i].specialSkill}</p>`;
+    
+    switch (pets[i].typeOfPet) {
+            case "Dog":  
+                domString += `<footer class="dog">${pets[i].typeOfPet}</footer>`;
+            break;
+            case "Cat":
+                domString += `<footer class="cat">${pets[i].typeOfPet}</footer>`;
+            break;
+            case "Dinosaur": 
+                domString += `<footer class="dinosaur">${pets[i].typeOfPet}</footer>`;
+            break;
+        }
+    domString += `</div>`;
     }
 printToDom('pet-adoption', domString);
 };
